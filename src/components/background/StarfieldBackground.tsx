b2333193f45ef3218447f1947fc3509c71245578
Juan Manuel Baez <juanmanuelbaez1994@gmail.com>
2025-01-27 22:43:31 -0300

    Background componentization

---
+++ src/components/background/StarfieldBackground.tsx
@@ -0,0 +1,10 @@
+// const StarfieldBackground = () => {
+//   return (
+//     <mesh>
+//       <points>
+//         <bufferGeometry />
+//         <pointsMaterial size={0.1} sizeAttenuation={true} />
+//       </points>
+//     </mesh>
+//   );
+// }
